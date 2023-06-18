2843dd221cd6361ee5e68a9fd67980f391180866
hassan <hassanbahmandar5@gmail.com>
2023-06-18 23:26:33 +0330

    add js file (traffic light)

---
+++ traffic light/script.js
@@ -0,0 +1,24 @@
+function redClick() {
+  r = document.getElementById("red");
+  y = document.getElementById("yellow");
+  g = document.getElementById("green");
+  r.style.backgroundColor = " #ff2530";
+  y.style.backgroundColor = " #ffa72480";
+  g.style.backgroundColor = " #00998280";
+}
+function greenClick() {
+  r = document.getElementById("red");
+  y = document.getElementById("yellow");
+  g = document.getElementById("green");
+  r.style.backgroundColor = " #ff242f80";
+  y.style.backgroundColor = " #ffa825";
+  g.style.backgroundColor = " #00998280";
+}
+function yellowClick() {
+  r = document.getElementById("red");
+  y = document.getElementById("yellow");
+  g = document.getElementById("green");
+  r.style.backgroundColor = " #ff242f80";
+  y.style.backgroundColor = " #ffa72480";
+  g.style.backgroundColor = " #009b83";
+}
